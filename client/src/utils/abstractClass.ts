91b87c7401b812b972ec7f0d77b37d31be711888
Vit K <vitaly.khe@gmail.com>
2021-12-06 17:49:06 +0400

    m

--- client/src/utils/abstractClass.ts
+++ client/src/utils/abstractClass.ts
@@ -1,7 +1,43 @@
-export class MD {
-  private connectionString: string
-  constructor(public url: string, public port: number) {
-    this.connectionString = url + port.toString()
+export class Department {
+  // private readonly id: string
+  // private name: string
+  private employees: string [] = []
+
+  constructor(private readonly id: string, public name: string) {
   }
 
+  describe(this: Department) {
+    console.log(`This dept name is ${this.name}. id: ${this.id}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  getEmployees() {
+    return this.employees
+  }
+}
+
+class ITDepartment extends Department {
+
+  admins: string[]
+  private numberOfEmpoyees: number
+
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT')
+    this.admins = admins
+  }
+}
+
+class AccountingDepartment extends Department {
+
+  constructor(id: string, private balance: Record<string, number>) {
+    super(id, 'Accounting')
+  }
+
+  showBalance() {
+    return this.balance
+  }
+  
 }
