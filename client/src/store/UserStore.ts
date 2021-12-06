5ee6e0be8ba8966a3ec90e48bae1c56b2888c9f4
Vit K <vitaly.khe@gmail.com>
2021-12-06 17:57:56 +0400

    null to undefined

--- client/src/store/UserStore.ts
+++ client/src/store/UserStore.ts
@@ -3,23 +3,23 @@ import { User } from '../utils/types'
 
 export default class UserStore {
     private _isAuth: boolean
-    private _user: User|null
+    private _user: User|undefined
     constructor(){
         this._isAuth = false
-        this._user = null
+        this._user = undefined
         makeAutoObservable(this)
     }
     setIsAuth(bool: boolean){
         this._isAuth = bool
     }
     //переопределит тип позже, когда будут известны все поля пользователя
-    setUser(user: User|null) { 
+    setUser(user: User|undefined) { 
         this._user = user
     }
     getIsAuth(): boolean {
         return this._isAuth
     }
-    getUser(): User|null {
+    getUser(): User|undefined {
         return this._user
     }
     getRole(): string {
